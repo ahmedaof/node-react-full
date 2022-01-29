51051b67896beb0386c8076a52aaf9f240fef9df
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2022-01-29 11:29:26 +0200

    enhance all app

--- react-front/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = ()=>(<div>hello feom react</div>)
-
-export default App;
--- react-front/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './Routes';
-
-ReactDOM.render(
-    <Router />,
-  document.getElementById('root')
-);
